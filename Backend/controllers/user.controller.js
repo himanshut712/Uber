12a39f3e16b1061b307fbb3112a5e129bcac6679
Himanshu Tilwani <harshtlwn@gmail.com>
2025-03-18 21:21:06 +0530

    captain registraion done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -13,6 +13,13 @@ console.log(req.body);
 
 const{fullname, email, password} = req.body;
 
+const isUserAlreadyExist = await userModel
+.findOne({email})
+
+if(isUserAlreadyExist){
+    return res.status(400).json({message: 'User already exist'})
+}
+
 const hashedPassword = await userModel.hashPassword(password)
 
 const user = await userService.creatUser({
